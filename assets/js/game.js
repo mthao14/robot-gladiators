c07e861262385258bade4e7ba5cd3a15c4c5d4c6
you@example.com <mthao14@outlook.com>
2022-01-02 21:26:49 -0600

    added integer input for prompts

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,9 +32,9 @@ var fightOrSkip = function() {
 
     // if yes (true), leave fight
     if (confirmSkip) {
-      window.alert(playInfo.name + " has decided to skip this fight. Goodbye!");
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
       // subtract money from playerMoney for skipping
-      playerInfor.money = Math.max(0, playerInfo.money = 10);
+      playerInfo.money = Math.max(0, playerInfo.money = 10);
 
       //retur true if player wants to leave
       return true;
@@ -155,21 +155,20 @@ endGame();
   var shop = function() {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     // use switch to carry out action
     switch (shopOptionPrompt) {
-      case "REFILL":
-      case "refill":
+      case 1:
         playerInfo.refillHealth();
         break;
-      case "UPGRADE": // new case
-      case "upgrade":
+      case 2:
         playerInfo.upgradeAttack();
         break;
-      case "LEAVE": // new case
-      case "leave":
+      case 3:
         window.alert("Leaving the store.");
         // do nothing, so function will end
         break;
